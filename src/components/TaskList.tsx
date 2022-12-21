14666e1902b5034a61e6759341885f98ce8726ef
Pedro Marques <pdro.marqs2015@hotmail.com>
2022-12-21 00:02:24 -0300

    desafio finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,38 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+    //imutabilidade, criando um "estado temporario" para armazenar as informações pedidas na interface, que vao para a array do estado.
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+    //esse old task é ele pegando tudo que ja tem no estado task e mantendo la usando o spredOperator, e adicionando o novo valor em newTask.
+    setTasks(oldTask => [...oldTask, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    // essa variavel pega a array tasks e mapeia. Cada task dentro dela passa por verificação de id pra ver se é igual ao selecionado. Se o id for igual ao selecionado, ele puxa pro objeto os ites de task com ..task e edita o isComplete para o inverso do valor atual. Caso o id seja diferente ele só retorna o item como estava antes.
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    //essa variavel armazena o resultado de um filtro em tasks que devolve todas as tasks que nao tem o mesmo ID do item selecionado.
+    const filteredTasks = tasks.filter(task => task.id !== id);
+
+    //salva o novo array de tasks que sao diferentes da com id selecionado. Ou seja, salva tudo menos a que foi removida.
+    setTasks(filteredTasks);
   }
 
   return (
@@ -32,14 +56,14 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
-            <FiCheckSquare size={16} color="#fff"/>
+            <FiCheckSquare size={16} color="#fff" />
           </button>
         </div>
       </header>
@@ -50,7 +74,7 @@ export function TaskList() {
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -62,11 +86,11 @@ export function TaskList() {
               </div>
 
               <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+                <FiTrash size={16} />
               </button>
             </li>
           ))}
-          
+
         </ul>
       </main>
     </section>
